dbe5d3e3370634107fe5d06cc790826eb449a604
AkankshaVPatil <akanksha.v.patil@gmail.com>
2025-07-12 08:29:15 +0530

    Create LinkedList_Prob2.java

---
+++ src/LinkedList_Prob2.java
@@ -0,0 +1,5 @@
+//https://leetcode.com/problems/delete-node-in-a-linked-list/description/
+public void deleteNode(ListNode node) {
+        node.val = node.next.val;
+        node.next = node.next.next;
+    }
